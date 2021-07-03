0b2b75243c1f463a330963f6cdcf4acb6f5d15f9
Your Name <you@example.com>
2021-07-03 11:11:30 +0300

    configured Images model

--- picasso/urls.py
+++ picasso/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from gallery import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.home, name='home')
+    path("",views.home, name='home'),
 ]
