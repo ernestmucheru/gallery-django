aca449d8e54dacb915c1bdfc06f3bcd1a3038cba
Your Name <you@example.com>
2021-07-03 11:06:49 +0300

    created an images model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+    alt = models.CharField(max_length=60)
+    image = models.Image(upload_to="")
+    
+    def __str__(self):
+        return self.alt
