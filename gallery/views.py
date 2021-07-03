a72c8ed50e0a582b5bff2cbf0b6ea710151e788a
Your Name <you@example.com>
2021-07-03 11:35:30 +0300

    linked gallery tmeplate to images model

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Images
 
 # Create your views here.
 def home(request):
-    
-    return render(request, 'index.html')
+    context = {}
+    images = Images.objects.all()
+    context["images"] = images
+
+        
+    return render(request, 'index.html',context)
--- picasso/urls.py
+++ picasso/urls.py
@@ -8,3 +8,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
