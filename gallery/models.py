bcf5484daf0e6452f02c3716d3f223da679e221c
Your Name <you@example.com>
2021-07-05 07:22:06 +0300

    added categories model and modified Images

--- gallery/models.py
+++ gallery/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100,null=False ,blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Images(models.Model):
     alt = models.CharField(max_length=60)
     image = models.ImageField(upload_to="")
+    description = models.TextField(max_length=500, null=False,blank=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
         return self.alt
