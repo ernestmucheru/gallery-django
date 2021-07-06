31de96c740011480e119c519e0ee91566eb47e43
Your Name <you@example.com>
2021-07-06 12:01:22 +0300

    deployment changes

--- picasso/settings.py
+++ picasso/settings.py
@@ -89,7 +89,7 @@ ROOT_URLCONF = 'picasso.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
